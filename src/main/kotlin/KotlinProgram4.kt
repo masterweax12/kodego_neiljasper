79440d695cc48ccabe1fb2df75bdf36fb2fbf284
Neil Jasper Juntilla <matthew261819@gmail.com>
2022-09-03 15:41:18 +0800

    KotlinProgram4Commit

--- src/main/kotlin/KotlinProgram4.kt
+++ src/main/kotlin/KotlinProgram4.kt
@@ -1,22 +1,23 @@
 fun main() {
-    var number: Int = 9
-
-    if (number < 10) {
-        println("Hello")
-    }
-
-    var grades: Int = 70
-    if (grades >=75) {
-        print("You passed!")
-    }
-
+    var grades: Int = 75
+    if ((grades >= 96)&&(grades <=100)) {
+        print("1.0")
+    }else if ((grades >=86)&&(grades <=95)){
+        println("1.75")
+    }else if ((grades >=76)&&(grades <=85)){
+        println ("2.25")
+    }else if (grades < 75 ){
+    println("5.0")
+    }else{
+    println("Invalid grade!")
+}
     //range
-    var idNumber : Int = 1
-    if((idNumber >=1)&&(idNumber <=3) ){
-        println("Valid Id number")
-        println("SUCCESS!!!")
-
-    }
-
-    println("Line Ends")
+//    var idNumber : Int = 1
+//    if((idNumber >=1)&&(idNumber <=3) ){
+//        println("Valid Id number")
+//        println("SUCCESS!!!")
+//
+//    }
+//
+//    println("Line Ends")
 }
