140c1cafae930359c69fe2a7b9ace397d7476917
Neil Jasper Juntilla <matthew261819@gmail.com>
2022-09-03 13:54:02 +0800

    ExcerciseActivity01

---
+++ src/main/kotlin/excercise_activities/ExerciseActivity01.kt
@@ -0,0 +1,21 @@
+package exercise_activities
+
+fun main() {
+    //Student lists 01-A
+    var student1: String = "This is student1\n"
+    var student2: String = "This is student2\n"
+    var student3: String = "This is student3\n"
+    var student4: String = "This is student4"
+    var studentList: String = student1 + student2 + student3 + student4
+    var progress: String = "\"Exercise\""
+    print("STUDENT DIRECTORY:\n")
+
+    println(studentList)
+
+    print("Student progress:$progress ")
+
+    //School Library 01-B
+
+    //Inventory System for grocery 01-C
+
+}
