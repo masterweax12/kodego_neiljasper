788656280448d1cdccb323f966bc4bfefd071c15
Neil Jasper Juntilla <matthew261819@gmail.com>
2022-09-03 16:10:30 +0800

    Commit else if else

---
+++ src/main/kotlin/KotlinProgram5.kt
@@ -0,0 +1,20 @@
+fun main(){
+    var itemNumber : Int = 2
+
+    when(itemNumber){
+        1 -> print("Clothes")
+        2 -> print("Food")
+        3 -> print ("Toiletries")
+        else -> print("invalid item number")
+    }
+
+    var gender : String = "X"
+    if (gender == "M") {
+        println("Male")
+    }else if (gender == "F"){
+        println("Female")
+    }else{
+        println("Invalid choice")
+    }
+
+}
