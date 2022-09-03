12573f659d09a782e562c1b995263774929185a6
Neil Jasper Juntilla <matthew261819@gmail.com>
2022-09-03 10:33:13 +0800

    KotlinProgram3

--- src/main/kotlin/KotlinProgram3.kt
+++ src/main/kotlin/KotlinProgram3.kt
@@ -1,8 +1,20 @@
 fun main(){
     var number1 : Int = 21
-    var number2 : Int = 9
+    var number2 : Int = 21
 
-    var answer : Int = number1 + number2
+    var answer : Int = number1 % number2
 
     println("Answer is $answer")
+
+    //complex operation
+    var complexFormula : Int =(2*10)*(10/2)
+    println(complexFormula)
+
+    //comparison operator
+    var expression : Boolean = !(number1 < number2 )
+    println(expression)
+
+    //increment operator
+    var incrementNum : Int = ++number1
+    println(incrementNum)
 }
