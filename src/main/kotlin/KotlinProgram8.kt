d8ebab5a408d6634a6903257d047db7fc5b26a47
Neil Jasper Juntilla <matthew261819@gmail.com>
2022-09-05 21:00:41 +0800

    KotlinProgram8

---
+++ src/main/kotlin/KotlinProgram8.kt
@@ -0,0 +1,17 @@
+fun main(){
+
+    //Increment ++ means add 1 -- means minus 1
+//    var ctr : Int = 8
+//    ctr++
+//    println(ctr)
+
+    //while loop
+    //init
+    var ctr : Int = 1
+    //condition: "Always looping" Common problem
+    while (ctr <=5 ){
+        println("Hello World!$ctr")
+        //update terminate or Stop the loop
+        ctr++
+    }
+}
