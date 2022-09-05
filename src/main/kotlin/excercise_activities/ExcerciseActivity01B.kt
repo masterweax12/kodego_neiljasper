4cff3e1a3392c6698f280746083510e71ae6745d
Neil Jasper Juntilla <matthew261819@gmail.com>
2022-09-05 14:47:47 +0800

    Exercise01B

---
+++ src/main/kotlin/excercise_activities/ExcerciseActivity01B.kt
@@ -0,0 +1,35 @@
+fun main(){
+
+    // School Library Intro
+    println("Hi, Welcome to our School Library! Here are our lists of available books to read or borrow. Check Below:\n")
+
+    //School Library Books
+    var bookNumber5 : Int = 5
+    var bookNumber4 : Int = 4
+    var bookNumber3 : Int = 3
+    var bookNumber2 : Int = 2
+    var bookNumber1 : Int = 1
+
+    //Book Stocks
+    var bookStocks5 : Int = 23
+    var bookStocks4 : Int = 21
+    var bookStocks3 : Int = 47
+    var bookStocks2 : Int = 12
+    var bookStocks1 : Int = 8
+
+    //Book Name
+    var bookName5 : String = "\"Daily Market\""
+    var bookName4 : String = "\"Forex Fundamentals\""
+    var bookName3 : String = "\"The Sentiments Report\""
+    var bookName2 : String = "\"Smart Money Concept\""
+    var bookName1 : String = "\"CandleSticks Chart Pattern\""
+
+    //Books Output display
+    println("Book Number $bookNumber5 with book name $bookName5 has currently $bookStocks5 Stocks available.")
+    println("Book Number $bookNumber4 with book name $bookName4 has currently $bookStocks4 Stocks available.")
+    println("Book Number $bookNumber3 with book name $bookName3 has currently $bookStocks3 stocks available.")
+    println("Book Number $bookNumber2 with book name $bookName2 has currently $bookStocks2 stocks available.")
+    println("Book Number $bookNumber1 with book name $bookName1 has currently $bookStocks1 stocks available.")
+
+
+}
