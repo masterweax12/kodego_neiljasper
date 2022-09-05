1f1aceded3368d6ad9bcaa6fd25efd9012ccf5f2
Neil Jasper Juntilla <matthew261819@gmail.com>
2022-09-05 17:11:46 +0800

    ExcerciseActivityC.kt

---
+++ src/main/kotlin/excercise_activities/ExcerciseActivity01C.kt
@@ -0,0 +1,32 @@
+fun main (){
+    println("Grocery Goods and Stock lists\n")
+
+    //Grocery Categories (Food)
+    println("Categories: Food")
+
+    //Categories for Food
+    var food1 : String = "Rice"
+    var food2 : String = "Fresh meat"
+    var food3 : String = "Bread"
+    var food4 : String = "Sugar"
+    var food5 : String = "Biscuit Snacks"
+
+    //Output Display for Food
+    println("$food1, $food2, $food3, $food4, $food5.\n")
+
+    //Grocery Categories (Non Food)
+    println("Categories: Non Food")
+
+    //Categories for Non Food
+    var nonFood1 : String = "Detergent"
+    var nonFood2 : String = "Sanitary"
+    var nonFood3 : String = "Plastic Containers"
+    var nonFood4 : String = "Utensils"
+    var nonFood5 : String = "Floor Wax"
+
+    //Output Display for Non Food
+    println("$nonFood1, $nonFood2, $nonFood3, $nonFood4, $nonFood5.")
+
+    //End of Output
+
+}
