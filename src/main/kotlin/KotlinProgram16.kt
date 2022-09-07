b8cfa75a8ae54bd4bd58380309b58c35396461ee
Neil Jasper Juntilla <matthew261819@gmail.com>
2022-09-07 20:44:36 +0800

    KotlinProgram16

---
+++ src/main/kotlin/KotlinProgram16.kt
@@ -0,0 +1,15 @@
+fun main(){
+    var names = ArrayList<String>()
+    names.add("John")       //index 0
+    names.add("Peter")      //index 1
+
+    //add elements
+//    println(names[0])
+//    println(names[1])
+
+    //remove elements
+    names.removeAt(0)
+
+    //display names
+    println(names[0])
+}
