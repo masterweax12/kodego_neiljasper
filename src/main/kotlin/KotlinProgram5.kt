c211cb8d99ba6abc9bf13ed53d15de96667083e6
Neil Jasper Juntilla <matthew261819@gmail.com>
2022-09-07 18:09:19 +0800

    KotlinProgram5

--- src/main/kotlin/KotlinProgram5.kt
+++ src/main/kotlin/KotlinProgram5.kt
@@ -1,20 +1,20 @@
 fun main() {
-    var itemNumber : Int = 4
-    when (itemNumber) {
-        1 -> print("Clothes")
-        2 -> print("Food")
-        3 -> print("Toiletries")
-        else -> print("invalid item number")
-    }
-//
-//    var gender : String = "X"
-//    if (gender == "M") {
-//        println("Male")
-//    }else if (gender == "F"){
-//        println("Female")
-//    }else{
-//        println("Invalid choice")
+//    var itemNumber : Int = 4
+//    when (itemNumber) {
+//        1 -> print("Clothes")
+//        2 -> print("Food")
+//        3 -> print("Toiletries")
+//        else -> print("invalid item number")
 //    }
-//
+
+    var gender : String = "M"
+    if (gender == "M") {
+        println("Male")
+    }else if (gender == "F"){
+        println("Female")
+    }else{
+        println("Invalid choice")
+    }
+
 //}
 }
