dd44cc26b852f010444245f9e65b85c46d92b01c
Neil Jasper Juntilla <matthew261819@gmail.com>
2022-09-07 20:51:25 +0800

    KotlinProgram16

--- src/main/kotlin/KotlinProgram16.kt
+++ src/main/kotlin/KotlinProgram16.kt
@@ -1,15 +1,30 @@
 fun main(){
     var names = ArrayList<String>()
-    names.add("John")       //index 0
-    names.add("Peter")      //index 1
-
-    //add elements
+//    names.add("John")       //index 0
+//    names.add("Peter")      //index 1
+//
+//    //add elements
 //    println(names[0])
 //    println(names[1])
+//
+//    //remove elements
+//    names.removeAt(0)
+//
+//    //display names
+//    println(names[0])
 
-    //remove elements
-    names.removeAt(0)
 
-    //display names
-    println(names[0])
+    //add names to arrayList
+    names.add("Mary")
+    names.add("Jenny")
+    names.add("Mark")
+    names.add("Paul")
+    names.add("Bryan")
+    names.add("Roel")
+
+    //traverse
+    for(value in names){
+        print(" $value ")
+    }
+
 }
