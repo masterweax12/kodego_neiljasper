e0c88f687b9a8a694a997f57c7ee2a11246667c0
Neil Jasper Juntilla <matthew261819@gmail.com>
2022-09-07 18:58:09 +0800

    ExcerciseActivity01E

---
+++ src/main/kotlin/excercise_activities/ExcerciseActivity01E.kt
@@ -0,0 +1,18 @@
+fun main(){
+
+    /* Create an application that will accept 2 string inputs.
+    Your application will print all unique characters in both Strings.
+     */
+    var stringUnique : String = "france"
+    var stringUnique2:  String = "glance"
+
+    println("Please Enter the First Word: ")
+    var firstString : String = readln().toString()
+    println(stringUnique.substring(0,2))
+
+    println("Please Enter the Second Word: ")
+    var secondString : String = readln().toString()
+    println(stringUnique2.substring(0,2))
+    println("\n")
+    println("The Unique Characters of \"France\" and \"Glance\" are \"fr\" and \"gl\"")
+}
