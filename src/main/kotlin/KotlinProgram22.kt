061b358c32582244b71f5678d6f8d86543dd66e4
Neil Jasper Juntilla <matthew261819@gmail.com>
2022-09-10 13:53:57 +0800

    KotlinProgram22

---
+++ src/main/kotlin/KotlinProgram22.kt
@@ -0,0 +1,41 @@
+fun main(){
+    displayMessage("My name is Jack!")
+//    println("Enter Number 1:")
+//    var number : Int = readln().toInt()
+//
+//    println("The cube is ${number * number * number}")
+//
+//    println("Enter Number 2:")
+//    var number2 : Int = readln().toInt()
+//
+//    println("The cube is ${number2 * number2 * number2}")
+
+}
+
+
+//no return function no parameter
+fun displayName(){
+    println("Peter")
+}
+//no return with 1 parameter
+fun displayMessage(msg: String){
+    println("The message is \"$msg\"")
+}
+
+
+
+//function-> methods-> stored procedure-> sub procedure
+
+//built-in/standard function in kotlin
+//abstraction
+
+//println()
+//toInt()
+//readln()
+//main()
+//arrayOf()
+//listOf()
+
+
+
+//user-defined function
