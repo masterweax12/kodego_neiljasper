69469b08829c6825469f6a3de8d2075759a4b6aa
Neil Jasper Juntilla <matthew261819@gmail.com>
2022-09-14 17:13:09 +0800

    KotlinProgram16

--- src/main/kotlin/KotlinProgram14.kt
+++ src/main/kotlin/KotlinProgram14.kt
@@ -9,11 +9,11 @@ fun main(){
     }
 
     //equivalent to 5 prints
-//    println(names[0])
-//    println(names[1])
-//    println(names[2])
-//    println(names[3])
-//    println(names[4])
+    println(names[0])
+    println(names[1])
+    println(names[2])
+    println(names[3])
+    println(names[4])
 
     //display new array values
     var ctr2 : Int = 0
--- src/main/kotlin/KotlinProgram16.kt
+++ src/main/kotlin/KotlinProgram16.kt
@@ -1,17 +1,17 @@
 fun main(){
     var names = ArrayList<String>()
-//    names.add("John")       //index 0
-//    names.add("Peter")      //index 1
-//
-//    //add elements
-//    println(names[0])
-//    println(names[1])
-//
-//    //remove elements
-//    names.removeAt(0)
-//
-//    //display names
-//    println(names[0])
+    names.add("John")       //index 0
+    names.add("Peter")      //index 1
+
+    //add elements
+    println(names[0])
+    println(names[1])
+
+    //remove elements
+    names.removeAt(0)
+
+    //display names
+    println(names[0])
 
 
     //add names to arrayList
