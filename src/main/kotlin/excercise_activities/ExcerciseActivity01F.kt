67617c335a6eefccb781c9deb52fcc8b37f7c0f8
Neil Jasper Juntilla <matthew261819@gmail.com>
2022-09-14 19:00:41 +0800

    ExcerciseActivity01F(Finalization)

--- src/main/kotlin/excercise_activities/ExcerciseActivity01F.kt
+++ src/main/kotlin/excercise_activities/ExcerciseActivity01F.kt
@@ -3,7 +3,7 @@ fun main() {
     /*
     Author Name: Neil Jasper A. Juntilla
     Date: 9/4/2022
-    File Name: ExcercieActivity01F.kt
+    File Name: ExcerciseActivity01F.kt
     */
 
     println("Enter a word to check if it's a Palindrome")
@@ -14,7 +14,8 @@ fun main() {
     //Logic If Else Condition
     println("Word in reversed: $secondWord")
     if ((word) == (secondWord)) {
-        println("This Word is a Palindrome!")
+
+        println("This word is a Palindrome!")
     }else{
         println("This word is not a Palindrome!")
     }
