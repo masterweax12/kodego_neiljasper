12bcdab4f64fe3bad990980933f4e1e3ef102411
Neil Jasper Juntilla <matthew261819@gmail.com>
2022-09-16 18:36:37 +0800

    PracticeActivity8

--- src/main/kotlin/practice_activities/PracticeActivity8.kt
+++ src/main/kotlin/practice_activities/PracticeActivity8.kt
@@ -1,12 +1,15 @@
 package PracticeActivity
-fun main(){
+fun main() {
 
-    println(maxNumber(1, 3, 5))
-
-}
-fun maxNumber(num1:Int, num2:Int, num3:Int):Int{
-    var highValue = 0
-    if
-
-
+//    println(maxNumber(1, 3, 5))
+//
+//}
+//fun maxNumber(num1:Int, num2:Int, num3:Int):Int{
+//    var highValue = 0
+//    if((num1 > num2 && num2 < num1))
+//        }
+//}
+//
+//
+//}
 }
