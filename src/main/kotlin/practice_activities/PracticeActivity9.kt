08ea0419cb64f211e6c21b370df10b6581992d5b
Neil Jasper Juntilla <matthew261819@gmail.com>
2022-09-16 22:05:56 +0800

    PracticeActivity9(updated)

--- src/main/kotlin/practice_activities/PracticeActivity9.kt
+++ src/main/kotlin/practice_activities/PracticeActivity9.kt
@@ -1,56 +1,44 @@
 package practice_activities
 fun main(){
-    var myPhone = XiaomiPhone()
-    myPhone.attributes()
-    println(myPhone.attributes())
-    myPhone.behaviors()
-    println(myPhone.behaviors())
-
+    var myPhone = PhoneClass()
+    myPhone.sendSMS()
+    myPhone.takePhotos()
+    myPhone.playRadio()
+    myPhone.gameTurbo()
+    myPhone.linuxOperate()
 
 }
-    //class section
-class XiaomiPhone{
-    var modelName: String = "Redmi 9T"
-    var operatingSystem: String = "Android"
-    var androidVersion: Int = 11
-
-    //initial values
-    init{
-        println("Object Created!")
+//class section
+class PhoneClass(var brand: String, var modelName: String, var os: String, var androidVersion: Int, var color: String){
+    init {
+        println("Brand: $brand")
         println("Model name: $modelName")
-        println("Operating system: $operatingSystem")
+        println("Operating system: $os")
         println("Android version: $androidVersion")
-
-    //attributes function
-    }
-    fun attributes(){
-        var miuiVersion: Int = 12
-        var ram: Int = 8
-        var rom: Int = 128
-        var clockSpeed: Double = 2.0
-        var inch: Double = 6.7
-        println("MIUI Version: $miuiVersion")
-        println("RAM: $ram")
-        println("ROM: $rom")
-        println("Clockspeed: $clockSpeed Ghz")
-        println("Inch Display: $inch inches")
+        println("Phone Color: $color")
     }
 
+    constructor(brand: String):this(brand = "OnePlus", "One Plus 9", "Android", 11, "Sky Blue"){}
+    constructor(androidVersion:Int):this("Oppo", "OppoF5s", "Android", androidVersion, "red"){}
+    constructor():this(""){}
     //behavior functions
-    fun behaviors(){
-        var fingerPrint: String = "Sidemounted"
-            println("Finger print $fingerPrint, runs smoothly estimated 50 milliseconds")
-        var AnTuTu: String = "177917"
-            println("Total benchmark score went up to $AnTuTu.")
-        var reverseCharging: Double = 2.5
-            println("Has a unique feature of Reverse Charging up to $reverseCharging")
-        var processor: String = "Qualcomm SM6115 Snapdragon 662 (11nm)"
-            println("Chipset runs on $processor.")
-        var wirelessTransaction: Boolean = true
-            println("Is NFC enabled? $wirelessTransaction")
-
-
+    fun sendSMS() {
+        println("This phone can send SMS.")
     }
+    fun takePhotos() {
+        println("This phone can take photos with a quality of 20MP.")
+    }
+    fun playRadio(){
+        println("This phone supports playing of FM Radio.")
+    }
+    fun gameTurbo() {
+        println("This phone can store games with it's unique feature of Game Turbo")
+    }
+    fun linuxOperate(){
+        println("This phone has an enabled option to operate linux.")
+    }
+
+
 
 }
-                        //end of code
+//end of code
