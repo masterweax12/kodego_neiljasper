0cc5cdaf181ac8d315a8a7bb3c89a38bf720420a
Neil Jasper Juntilla <matthew261819@gmail.com>
2022-09-17 10:01:14 +0800

    KotlinProgram29

---
+++ src/main/kotlin/KotlinProgram29.kt
@@ -0,0 +1,32 @@
+fun main(){
+
+}
+
+class Empty{}
+
+class Outer{
+
+    val name = "Outer"
+    fun show() = "the name: $name"
+
+    class Nested{
+        val name = "Nested"
+        fun show() = "the name: $name"
+    }
+}
+
+class Outer2{
+
+    val name1 = "Outer2"
+    fun show() = "the name: $name1"
+
+    inner class Inner{
+        val name2 = "Inner"
+        fun show() = "data: $name2 and $name1"
+    }
+}
+
+//sealed class
+//open class
+//abstract class
+//data class
