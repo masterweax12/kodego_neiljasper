a5ee354289a6427163b4c4381eeefd1f5311ce25
Neil Jasper Juntilla <matthew261819@gmail.com>
2022-09-18 10:27:06 +0800

    KotlinProgram30

---
+++ src/main/kotlin/KotlinProgram30.kt
@@ -0,0 +1,20 @@
+fun main(){
+
+    var num1: Int = 2
+    var num2: Int = 3
+
+    var sum: Int = num1 + num2
+    println("The sum of $num1 and $num2 is $sum")
+
+    var ctr: Int = 1
+    while(ctr <=3){
+        var ctr2: Int = 1
+        while(ctr2 <= 3){
+            println("@")
+            ctr2++
+        }
+        println()
+        ctr++
+    }
+
+}
