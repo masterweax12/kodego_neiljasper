f38fe8f352834a8eaed29506786b199e80dd5675
Neil Jasper Juntilla <matthew261819@gmail.com>
2022-09-18 10:34:10 +0800

    PracticeActivity2

---
+++ src/main/kotlin/practice_activities/PracticeActivity2.kt
@@ -0,0 +1,9 @@
+fun main(){
+    //Input Grades per subject
+    println("Enter Your Grade in Math:")
+    var mathGrade : Int = readln().toInt()
+    println("Enter Your Grade in English:")
+    var englishGrade : Int = readln().toInt()
+    println("Enter Your Grade in Filipino:")
+    var filipinoGrade: Int = readln().toInt()
+}
