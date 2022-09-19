a3a26e42c8042ff69b50c5808c51a38954e11cab
Neil Jasper A. Juntilla <matthew261819@gmail.com>
2022-09-19 13:52:19 +0800

    ExcerciseActivity02C

---
+++ src/main/kotlin/excercise_activities/ExcerciseActivity02C.kt
@@ -0,0 +1,27 @@
+package excercise_activities
+fun main(){
+
+    /*
+Author Name: シ Neil Jasper A. Juntilla シ
+Date Created: September 18, 2022
+File Name: ExcerciseActivity02C.kt
+ */
+
+    println("Groceries Goods and Stocks Lists\n")
+    var cat = arrayOf<String> ("\"Food\"", "\"Non Food\"")
+
+    //categories for food
+    println("Categories: ${cat[0]}")
+    var foods = arrayOf<String> ("Rice", "Fresh meat", "Bread", "Sugar", "Biscuit", "Snacks")
+    println("${foods[0]}, ${foods[1]}, ${foods[2]}, ${foods[3]}, ${foods[4]}, ${foods[5]}")
+
+    //categories for non food
+    println("\nCategories: ${cat[1]}")
+    var nonFoods = arrayOf<String> ("Detergent", "Sanitary", "Plastic Containers", "Utensils", "Floor Wax")
+    println("${nonFoods[0]}, ${nonFoods[1]}, ${nonFoods[2]}, ${nonFoods[3]}, ${nonFoods[4]}, ")
+
+    println("\nAll goods are in stock. Please Enjoy your Shopping シ ")
+    println("\nThank you and come again! シ")
+
+    //end of code
+}
