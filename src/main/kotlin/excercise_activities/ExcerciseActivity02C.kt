d9452d87c5fed0675e2027a492cee83feb877444
Neil Jasper A. Juntilla <matthew261819@gmail.com>
2022-09-21 21:35:29 +0800

    KotlinProgram34

--- src/main/kotlin/excercise_activities/ExcerciseActivity02C.kt
+++ src/main/kotlin/excercise_activities/ExcerciseActivity02C.kt
@@ -9,16 +9,21 @@ File Name: ExcerciseActivity02C.kt
 
     println("Groceries Goods and Stocks Lists\n")
     var cat = arrayOf<String> ("\"Food\"", "\"Non Food\"")
+    for (category in cat)
+        println(category)
 
     //categories for food
     println("Categories: ${cat[0]}")
     var foods = arrayOf<String> ("Rice", "Fresh meat", "Bread", "Sugar", "Biscuit", "Snacks")
-    println("${foods[0]}, ${foods[1]}, ${foods[2]}, ${foods[3]}, ${foods[4]}, ${foods[5]}")
+    for(food in foods){
+        println(food)
+    }
 
     //categories for non food
     println("\nCategories: ${cat[1]}")
     var nonFoods = arrayOf<String> ("Detergent", "Sanitary", "Plastic Containers", "Utensils", "Floor Wax")
-    println("${nonFoods[0]}, ${nonFoods[1]}, ${nonFoods[2]}, ${nonFoods[3]}, ${nonFoods[4]}, ")
+    for (nonFood in nonFoods)
+        println(nonFood)
 
     println("\nAll goods are in stock. Please Enjoy your Shopping シ ")
     println("\nThank you and come again! シ")
