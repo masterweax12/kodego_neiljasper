f4d7c4e65b81e71a99b671f52fb0ed9abbdd3298
Neil Jasper A. Juntilla <matthew261819@gmail.com>
2022-09-24 12:22:26 +0800

    PersonForOOP

--- src/main/kotlin/com/oop/Person.kt
+++ src/main/kotlin/com/oop/Person.kt
@@ -1,4 +1,38 @@
 package com.oop
+//Abstraction
+open class Person {
+    var name: String = "John"
+        get(){return field}
+        set(value){
+            field = value
+        }
 
-class Person {
+    var age: Int = 23
+        get(){return field}
+        set(value) {
+            if(value < 1){
+                println("Invalid Age!")
+            }else{
+                field = value
+            }
+            field = value
+        }
+
+    init{
+        println("Object Created!")
+        println("name is $name, age is $age")
+    }
+
+    fun uPAge():Int{
+        age++
+        return age
+    }
+
+    fun change(age:Int){
+        println(age)
+    }
+    open fun displayName(){
+        println("Name is $name")
+    }
+
 }
